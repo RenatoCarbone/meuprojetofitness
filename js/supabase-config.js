c1cac33b4c48147cbe7a86a47b99d3b3da456527
MiPlanFit <miplanfit@deploy.com>
2026-08-06 14:57:11 +0200

    config: update SITE_URL to https://miplanfit.pages.dev

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL  = 'https://itiqhoyxfsrzbyeejtdn.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0aXFob3l4ZnNyemJ5ZWVqdGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5MjI3MzUsImV4cCI6MjEwMTQ5ODczNX0.msAe5xQPgvWPPp6zpP8zgptem5FcnkLhU4x9ovYVzPY';
-const SITE_URL      = 'https://meuprojetofitness.pages.dev';
+const SITE_URL      = 'https://miplanfit.pages.dev';
 
 let _supabaseClient = null;
 
